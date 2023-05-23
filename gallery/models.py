e90b893be63ef29995594e21407b650c0737b532
Arseniy Boldurev <arsenijbold2003@gmail.com>
2023-05-23 10:28:55 +0300

    tasks and tests

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category, blank=True)
+    created_date = models.DateField()
+    age_limit = models.PositiveIntegerField()
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Image, Category
+from datetime import date
+
+class CategoryModelTest(TestCase):
+    def test_name_field(self):
+        category = Category(name='Test Category')
+        category.save()
+        saved_category = Category.objects.get(pk=category.pk)
+        self.assertEqual(saved_category.name, 'Test Category')
+
+class ImageModelTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Nature')
+        self.image = Image.objects.create(
+            title='Beautiful Landscape',
+            image='path/to/image.jpg',
+            created_date=date.today(),
+            age_limit=18
+        )
+        self.image.categories.add(self.category)
+
+    def test_image_title(self):
+        self.assertEqual(self.image.title, 'Beautiful Landscape')
+
+    def test_image_categories(self):
+        self.assertEqual(self.image.categories.count(), 1)
+        self.assertEqual(self.image.categories.first(), self.category)
+
+    def test_image_created_date(self):
+        self.assertEqual(self.image.created_date, date.today())
+
+    def test_image_age_limit(self):
+        self.assertEqual(self.image.age_limit, 18)
+
